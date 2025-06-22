8d3f3f5b1daae04960b7788164bc91b62d6c9aa3
Peppo94 <giuseppepotenza2594@gmail.com>
2025-06-22 09:04:15 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+// Import only if you're using a build system. For plain HTML, use the script CDN in index.html instead.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9jd74SINKKmkHhmvQfTx0nKiL7NTUF8w",
+  authDomain: "tunera-b3efa.firebaseapp.com",
+  projectId: "tunera-b3efa",
+  storageBucket: "tunera-b3efa.appspot.com",
+  messagingSenderId: "4289124219",
+  appId: "1:4289124219:web:507e539fe707e35b814fde",
+  measurementId: "G-0HLQQKY6L5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
